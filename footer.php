49de098dcf56fb8eb211dfa45e2c15153eaf3bff
Harshana ratnayaka <huratnayaka@gmail.com>
2020-12-02 01:24:43 +0530

    seperate footer

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+  <div class="d-sm-flex justify-content-center justify-content-sm-between">
+    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a
+        href="https://www.bootstrapdash.com/" target="_blank">bootstrapdash.com </a>2020</span>
+    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">
+      Made with &#128154; by
+      <a href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank"> Dreeko Corporations
+      </a>
+    </span>
+  </div>
+</footer>
